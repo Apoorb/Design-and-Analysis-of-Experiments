26c6e7cb2516fbdfa96b370ddd99075bc456c3af
Apoorba Bibeka <apoorb2510@gmail.com>
2018-10-15 15:46:16 -0500

    Copy files to a folder

--- BatchReplaceFileName.py
+++ BatchReplaceFileName.py
@@ -6,6 +6,8 @@ Created on Mon Oct 15 15:08:22 2018
 """
 
 import os
+import shutil
+import re
 
 print('Current working directory ',os.getcwd())
 os.chdir('C:/Users/a-bibeka/Dropbox/TTI_Projects/Road User Cost/VISSIM AM Peak V10/NB/NB 2025') 
@@ -14,3 +16,16 @@ print('Current working directory ',os.getcwd())
 #[os.rename(f, f.replace('_', '-')) for f in os.listdir('.') if not f.startswith('.')]
 # Replace the year from 2045 to 2025
 [os.rename(f, f.replace('2045', '2025')) for f in os.listdir('.') if not f.startswith('.')]
+
+#######################################################################################
+#Copying files to a folder
+path ="C:/Users/a-bibeka/Dropbox/TTI_Projects/Road User Cost/VISSIM AM Peak V10/NB/"
+OriginPath =os.path.join(path,'NB 2045')
+DestinPath=os.path.join(path,'NB 2025')
+files= os.listdir(OriginPath)
+files1 = [file for file in files if re.search(r'2045',file, re.I|re.M)]
+#With rbc files 
+#files1 = [file for file in files if re.search(r'2025|.rbc',file, re.I|re.M)]
+
+for f in files1:
+    shutil.copy(os.path.join(OriginPath,f), DestinPath)
